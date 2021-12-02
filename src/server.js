68fb5bb3b5744dd1045c9275b15b27d90060dffe
srpadilla <srpadilla@misena.edu.co>
2021-12-02 08:32:43 -0500

    primer commit antes de subir a github

---
+++ src/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+    auth_api_url:'https://samu-mision-tic-auth-ms.herokuapp.com',
+    account_api_url:'https://samu-mision-tic-account-ms.herokuapp.com',
+    /*auth_api_url: 'https://mision-tic-auth-ms.herokuapp.com',
+    account_api_url: 'https://mision-tic-account-ms.herokuapp.com',*/    
+    };
